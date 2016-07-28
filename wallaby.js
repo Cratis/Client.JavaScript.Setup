262457a56b9228aaba6de38821bd241505f8f34f
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-28 16:54:36 -0700

    Fixing the path to sinon -> pkg folder

--- wallaby.js
+++ wallaby.js
@@ -9,7 +9,7 @@ module.exports = function (wallaby) {
             { pattern: "jspm_packages/system.js", instrument: false },
             { pattern: "node_modules/chai/chai.js", instrument: false },
             { pattern: "node_modules/chai-as-promised/chai-as-promised.js", instrument: false },
-            { pattern: "node_modules/sinon/lib/sinon.js", instrument: false },
+            { pattern: "node_modules/sinon/pkg/sinon.js", instrument: false },
             { pattern: "jspm.config.js", instrument: false },
             { pattern: "Modules/JavaScript.Pipeline/json.js", instrument: false, load: false },
             { pattern: "package.json", instrument: false, load: false },
