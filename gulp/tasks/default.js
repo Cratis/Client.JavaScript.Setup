348f51f6e736ed07e2fbcc80047608a8b5e41fa6
Einar Ingebrigtsen <einar@dolittle.com>
2016-08-09 15:10:24 +0200

    Making  everything run in correct sequence

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -4,21 +4,22 @@ import "./html";
 import "./javascript";
 import "./less";
 import "./staticContent";
+import "./dotnet";
 import "./watch";
 
-gulp.task("tasks", ["html", "javascript", "less", "staticContent","watch"]);
+gulp.task("initialize", ["html", "javascript", "less", "staticContent"]);
 
-gulp.task("initialize", () => {
+gulp.task("printConfig", () => {
     console.log("**** Configuration ****")
     console.log(`Root folder : ${config.paths.rootDir}`)
     console.log(`Source folder : ${config.paths.sourceDir}`)
     console.log(`Outputting all files to : ${config.paths.outputDir}`)
     console.log("**** Configuration ****\n")
-
-    gulp.start("tasks");
 });
 
-gulp.task("default", ["initialize"]);
+gulp.task("default", () => {
+    runSequence(printConfig, initialize, ["watch", "dotnet"]);
+});
 
 export default {
     get config() {
